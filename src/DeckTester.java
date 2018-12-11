e512cc756d52c18930059e84c4f4f31c9c6c5fca
BT_1N3_02 <@>
2018-12-11 09:20:54 -0500

    a

---
+++ src/DeckTester.java
@@ -0,0 +1,12 @@
+public class DeckTester
+{
+    public static void main(String[] args)
+    {
+        String[] rank = {"Ace","1","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+        String[] suit = {"Spades", "Hearts", "Clubs", "Diamonds"};
+        int[] pointValue ={1,2,3,4,5,6,7,8,9,10,11,12,13};
+
+    }
+
+}
+
